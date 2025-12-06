adc9ac1f58aafc9cffa736fbb08c64110b3cd91c
Lucas Kent <rubickent@gmail.com>
2025-12-06 20:36:11 +1100

    Avoid collisions with user struct field names

--- kdl_config_derive/src/kdl_config.rs
+++ kdl_config_derive/src/kdl_config.rs
@@ -14,29 +14,31 @@ pub fn generate(input: DeriveInput) -> Result<TokenStream2, syn::Error> {
                     .map(|x| stringcase::kebab_case(&x.to_string()));
                 Ok(quote! {
                     impl KdlConfig for #ident {
-                        fn parse_as_node(input: NamedSource<String>, node: &KdlNode, diag: &mut Vec<kdl_config::error::ParseDiagnostic>) -> Parsed<#ident> {
+                        // arguments are prefixed with a random guid to ensure they dont collid with user field names.
+                        // Its silly but I'm not aware of a better solution
+                        fn parse_as_node(c068528d5bea4f73bf39204d30e57322_input: NamedSource<String>, c068528d5bea4f73bf39204d30e57322_node: &KdlNode, c068528d5bea4f73bf39204d30e57322_diag: &mut Vec<kdl_config::error::ParseDiagnostic>) -> Parsed<#ident> {
                             if let [
                                 #(Some(#rust_field_names),)*
                             ] = kdl_config::parse_helpers::get_children(
-                                input.clone(),
-                                node,
+                                c068528d5bea4f73bf39204d30e57322_input.clone(),
+                                c068528d5bea4f73bf39204d30e57322_node,
                                 [ #(#kdl_field_names,)* ],
-                                diag,
+                                c068528d5bea4f73bf39204d30e57322_diag,
                             ) {
                                 return Parsed {
                                     value: #ident {
-                                        #(#rust_field_names: KdlConfig::parse_as_node(input.clone(), #rust_field_names, diag),)*
+                                        #(#rust_field_names: KdlConfig::parse_as_node(c068528d5bea4f73bf39204d30e57322_input.clone(), #rust_field_names, c068528d5bea4f73bf39204d30e57322_diag),)*
                                     },
-                                    full_span: node.span(),
-                                    name_span: node.span(),
+                                    full_span: c068528d5bea4f73bf39204d30e57322_node.span(),
+                                    name_span: c068528d5bea4f73bf39204d30e57322_node.span(),
                                     valid: true,
                                 }
                             }
                             else {
                                 return Parsed {
                                     value: Default::default(),
-                                    full_span: node.span(),
-                                    name_span: node.span(),
+                                    full_span: c068528d5bea4f73bf39204d30e57322_node.span(),
+                                    name_span: c068528d5bea4f73bf39204d30e57322_node.span(),
                                     valid: false,
                                 }
                             }
@@ -64,24 +66,24 @@ pub fn generate(input: DeriveInput) -> Result<TokenStream2, syn::Error> {
                 .collect();
             Ok(quote! {
                 impl KdlConfig for #ident {
-                    fn parse_as_node(input: NamedSource<String>, node: &KdlNode, diagnostics: &mut Vec<kdl_config::error::ParseDiagnostic>) -> Parsed<#ident> {
+                    fn parse_as_node(c068528d5bea4f73bf39204d30e57322_input: NamedSource<String>, c068528d5bea4f73bf39204d30e57322_node: &KdlNode, c068528d5bea4f73bf39204d30e57322_diagnostics: &mut Vec<kdl_config::error::ParseDiagnostic>) -> Parsed<#ident> {
                         use kdl::KdlValue;
                         use kdl_config::parse_helpers::get_single_argument_value;
                         let kdl_names = [#(#kdl_names,)*];
-                        match get_single_argument_value(input.clone(), node, diagnostics) {
+                        match get_single_argument_value(c068528d5bea4f73bf39204d30e57322_input.clone(), c068528d5bea4f73bf39204d30e57322_node, c068528d5bea4f73bf39204d30e57322_diagnostics) {
                             Some(KdlValue::String(string)) => match string.as_str() {
                                 #(
                                     #kdl_names => Parsed {
                                         value: #ident::#variant_idents,
-                                        full_span: node.span(),
-                                        name_span: node.span(),
+                                        full_span: c068528d5bea4f73bf39204d30e57322_node.span(),
+                                        name_span: c068528d5bea4f73bf39204d30e57322_node.span(),
                                         valid: false,
                                     },
                                 )*
                                 name => {
-                                    diagnostics.push(kdl_config::error::ParseDiagnostic {
-                                        input: input.clone(),
-                                        span: node.span(),
+                                    c068528d5bea4f73bf39204d30e57322_diagnostics.push(kdl_config::error::ParseDiagnostic {
+                                        input: c068528d5bea4f73bf39204d30e57322_input.clone(),
+                                        span: c068528d5bea4f73bf39204d30e57322_node.span(),
                                         message: Some(format!(
                                             "Unknown value {name}"
                                         )),
@@ -91,16 +93,16 @@ pub fn generate(input: DeriveInput) -> Result<TokenStream2, syn::Error> {
                                     });
                                     Parsed {
                                         value: Default::default(),
-                                        full_span: node.span(),
-                                        name_span: node.span(),
+                                        full_span: c068528d5bea4f73bf39204d30e57322_node.span(),
+                                        name_span: c068528d5bea4f73bf39204d30e57322_node.span(),
                                         valid: false,
                                     }
                                 }
                             },
                             Some(name) => {
-                                diagnostics.push(kdl_config::error::ParseDiagnostic {
-                                    input: input.clone(),
-                                    span: node.span(),
+                                c068528d5bea4f73bf39204d30e57322_diagnostics.push(kdl_config::error::ParseDiagnostic {
+                                    input: c068528d5bea4f73bf39204d30e57322_input.clone(),
+                                    span: c068528d5bea4f73bf39204d30e57322_node.span(),
                                     message: Some(format!(
                                         "Expected type string but was {}", "TODO"
                                     )),
@@ -110,15 +112,15 @@ pub fn generate(input: DeriveInput) -> Result<TokenStream2, syn::Error> {
                                 });
                                 Parsed {
                                     value: Default::default(),
-                                    full_span: node.span(),
-                                    name_span: node.span(),
+                                    full_span: c068528d5bea4f73bf39204d30e57322_node.span(),
+                                    name_span: c068528d5bea4f73bf39204d30e57322_node.span(),
                                     valid: false,
                                 }
                             }
                             None => Parsed {
                                 value: Default::default(),
-                                full_span: node.span(),
-                                name_span: node.span(),
+                                full_span: c068528d5bea4f73bf39204d30e57322_node.span(),
+                                name_span: c068528d5bea4f73bf39204d30e57322_node.span(),
                                 valid: false,
                             }
                         }
